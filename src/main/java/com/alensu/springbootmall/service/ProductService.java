dfb33b4f94a35f32ead18f433b5cf6564a366fc1
HomeRight <alen40817a@gmail.com>
2022-06-10 13:23:19 +0800

    商品功能 -  實作修改商品

--- src/main/java/com/alensu/springbootmall/service/ProductService.java
+++ src/main/java/com/alensu/springbootmall/service/ProductService.java
@@ -2,10 +2,17 @@ package com.alensu.springbootmall.service;
 
 import com.alensu.springbootmall.dto.ProductRequest;
 import com.alensu.springbootmall.model.Product;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.validation.Valid;
 
 public interface ProductService {
 
     Product getProductById(Integer id);
 
     Integer createProduct(ProductRequest productRequest);
+
+    void updateProduct(Integer productId, ProductRequest productRequest);
 }
